fe11a9592abd1695e82e606810f6ee77ce1b7499
Sen Hung Wong <alexwongsenhung@gmail.com>
2023-07-21 21:37:10 -0700

    listen backend + new package.json

--- api/index.js
+++ api/index.js
@@ -42,4 +42,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+app.listen(process.env.PORT || "8888", () => {
+  console.log(`Server running on port ${process.env.PORT || "8888"}`);
+});
+
 module.exports = app;
